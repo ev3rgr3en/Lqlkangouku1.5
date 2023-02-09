2d561937e9538c2190749aa61500abd87f129026
ryu.utaku@growtech.co.jp <ryu.utaku>
2023-02-09 15:41:58 +0900

    111

--- app/src/main/java/com/gauravk/bubblebarsample/SettingActivity.kt
+++ app/src/main/java/com/gauravk/bubblebarsample/SettingActivity.kt
@@ -1,11 +1,9 @@
 package com.gauravk.bubblebarsample
 
 import android.annotation.SuppressLint
-import android.content.Context
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.text.Editable
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -14,32 +12,44 @@ import kotlinx.android.synthetic.main.fragment_screen_slide_page.*
 
 @SuppressLint("WrongViewCast")
 class SettingActivity : AppCompatActivity() {
-    private lateinit var btn : Button
-    var a=0
+    private lateinit var btn: Button
+    private var a = 0
+    private var num: Int = 0
+    private var days: Int = 0
+    private var inp: Int = 0
+
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_setting)
         val tv: TextView? = findViewById(R.id.textView)
         val tvp: TextView? = findViewById(R.id.predict_text)
-        val bundle:Bundle?=intent.extras
-        val num: Int =bundle!!.getInt("number")
-        tv?.text = "词汇数量:"+num
-        tvp?.text="预测学习天数:"+a
-//       inflatedView.setBackgroundColor(ContextCompat.getColor( this, #e57373))
-
-        btn=findViewById(R.id.setting_button)
-        var input= findViewById<EditText>(R.id.textInputEditText)
-        val inp=input.text
-        setting_button.setOnClickListener() {
-            val days:Int= num/inp
+        val bundle: Bundle? = intent.extras
+        val enteredString = readLine()?.toInt()
+        val input = findViewById<EditText>(R.id.textInputEditText)
+        inp = input.text.get().toInt()
+        num = bundle!!.getInt("number")
+        tv?.text = "词汇数量:$num"
+        tvp?.text = "预测学习天数:"+num/ enteredString!!
+        btn = findViewById(R.id.setting_button)
+//        val input = findViewById<EditText>(R.id.textInputEditText)
+//        inp =input.text.toInt()
+        setting_button.setOnClickListener {
+            days = num / enteredString
             val intent = Intent()
             intent.setClass(this, PlanActivity::class.java)
             intent.putExtra("days", days)
-            intent.putExtra("number",num)
+            intent.putExtra("number", num)
             startActivity(intent)
         }
+//       inflatedView.setBackgroundColor(ContextCompat.getColor( this, #e57373))
+
+
     }
+
+}
+
+
 //    fun days(){
 //        var input= findViewById<EditText>(R.id.textInputEditText)
 //        val inp=input.text
@@ -63,11 +73,8 @@ class SettingActivity : AppCompatActivity() {
 //            startActivity(intent)
 //        }
 //    }
-}
 
-private operator fun Int.div(inp: Editable?): Int {
-        return (1)
-}
+
 
 
 
